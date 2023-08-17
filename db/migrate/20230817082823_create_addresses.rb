0180f0859dfcc45218baae5b9f43d9c2c0517a5b
EC2 Default User <ec2-user@ip-172-31-19-208.ap-northeast-1.compute.internal>
2023-08-17 08:43:29 +0000

    [Add]item-model

--- db/migrate/20230817082823_create_addresses.rb
+++ db/migrate/20230817082823_create_addresses.rb
@@ -5,6 +5,7 @@ class CreateAddresses < ActiveRecord::Migration[6.1]
       t.string :name, null: false
       t.string :post_code, null: false
       t.string :address, null: false
+      
       t.timestamps
     end
   end
