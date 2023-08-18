468cc4a76fa6925714643a95fa1d2ece64b6b2ec
EC2 Default User <ec2-user@ip-172-31-12-31.ap-northeast-1.compute.internal>
2023-08-18 13:23:57 +0000

    [Add]enum-making_status

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,6 @@ class Order < ApplicationRecord
   has_many :order_items
   
   enum payment_method: {credit_card: 0, transfer: 1}
+  enum status: {waiting_for_payment: 0, confirmed_payment: 1, making: 2, shipping_preparation: 3, shipped: 4}
   
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,6 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
   
+  enum making_status:{not_ready: 0, pending: 1, making: 2, maked: 3}
+  
 end
