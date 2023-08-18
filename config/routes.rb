78ff0b7b1cea14cd03ace4c2609e427eabc719a5
horiren <138359070+horiren@users.noreply.github.com>
2023-08-18 05:38:08 +0000

    [Add]routing-public

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,33 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   
+  scope module: :public do
+    get '/' => 'homes#top'
+    get '/about' => 'homes#about'
+    
+    resources :items, only: [:index, :show]
+    
+    resource :customers, only: [:show, :edit, :update] do
+      collection do
+        get 'confirm_withdraw'
+        patch 'withdraw'
+      end
+    end
+    
+    resources :cart_items, only: [:index, :update, :destroy, :create] do
+      collection do
+        delete 'destroy_all'
+      end
+    end
+    
+    resources :orders, only: [:new, :index, :show, :create] do
+      collection do
+        post 'confirm'
+        get 'complete'
+      end
+    end
+    
+    resources :addresses, except: [:new, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
