dbfefb7d8f1ce4a32f2c0d1c3afcb0ca3ad54769
EC2 Default User <ec2-user@ip-172-31-19-208.ap-northeast-1.compute.internal>
2023-08-19 08:50:19 +0000

    halfway

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -3,7 +3,12 @@
 class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
+  
+  def after_sign_up_path_for(resource)
+    customers_path(resource)
+  end
+  
+  
   # GET /resource/sign_up
   # def new
   #   super
@@ -59,4 +64,13 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :is_admission])
+  end
 end
+
