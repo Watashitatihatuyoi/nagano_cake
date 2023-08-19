14428bd4b87acdac76dc5ddc71c31af90f79a401
EC2 Default User <ec2-user@ip-172-31-12-31.ap-northeast-1.compute.internal>
2023-08-19 06:13:25 +0000

    [Add]public-controller-order-new-confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,11 +1,24 @@
 class Public::OrdersController < ApplicationController
   
   def new
-    
+    @order = Order.new
   end 
   
   def confirm
-    
+    @order = Order.new(order_params)
+    if params[:order][:address_select] == 'own_address'
+      @order.post_code = current_customer.post_code
+      @order.address = current_customer.address
+      @order.name = current_customer.name
+    elsif params[:order][:address_select] == 'registered_address'
+      @address = Address.find(params[:order][:address_id])
+      @order.post_code = @address.post_code
+      @order.address = @address.address
+      @order.name = @address.name
+    elsif params[:order][:address_select] == 'new_address'
+      @order = Order.new(order_params)
+    end
+    @cart_items = current_customer.cart_items.all
   end 
   
   def complete
@@ -22,6 +35,12 @@ class Public::OrdersController < ApplicationController
   
   def show
     
+  end
+  
+  private
+  
+  def order_params
+    params.require[:order].permit[:payment_method, :post_code, :address, :name]
   end
   
 end
--- db/migrate/20230817091926_create_order_items.rb
+++ db/migrate/20230817091926_create_order_items.rb
@@ -1,7 +1,7 @@
 class CreateOrderItems < ActiveRecord::Migration[6.1]
   def change
     create_table :order_items do |t|
-      t.integer :oder_id, null: false
+      t.integer :order_id, null: false
       t.integer :item_id, null: false
       t.integer :quantity, null: false
       t.integer :price, null: false
