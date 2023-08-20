6b100e7a1cf072d899169056e3c4fdc44f08fac4
horiren <138359070+horiren@users.noreply.github.com>
2023-08-20 09:31:16 +0000

    [Add]cart_item-views

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,11 +1,16 @@
 class Public::CartItemsController < ApplicationController
   
   def index
-    
+    @cart_items = current_customer.cart_items.all
   end 
   
   def update
-    
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to cart_path, notice: "Quantity updated successfully."
+    else
+      render :edit
+    end
   end 
   
   def destroy
