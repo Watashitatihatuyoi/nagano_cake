ed4657bd4390208164911e34291a2bdadcfb516b
EC2 Default User <ec2-user@ip-172-31-31-207.ap-northeast-1.compute.internal>
2023-08-22 06:14:50 +0000

    マージのためのコミット

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -7,7 +7,9 @@ class Public::ItemsController < ApplicationController
   end
   
   def show
-    
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+    @cart_item = CartItem.new
   end
   
 end
