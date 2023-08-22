99ee3dc66789554b1b188c7eb0911c68218affe5
horiren <138359070+horiren@users.noreply.github.com>
2023-08-22 05:11:44 +0000

    [Add]admin-views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-    case resouce
+    case resource
     when Admin
-      admin_items_path
+      admin_path
     end
   end
 end
