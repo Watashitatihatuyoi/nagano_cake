e8fcbcc90a5fede1596e207dff14428d3f29c382
horiren <138359070+horiren@users.noreply.github.com>
2023-08-22 06:58:52 +0000

    [Add]cart_item-views

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,27 +1,32 @@
 class Public::CartItemsController < ApplicationController
   
   def index
-    @cart_items = current_customer.cart_items.all
-    @items = Item.all
+    if current_customer
+      @cart_items = current_customer.cart_items.all
+      @items = Item.all
+    else
+      flash[:notice] = "ログインしてください。"
+      redirect_to new_customer_registration_path
+    end
   end 
   
   def update
     @cart_item = CartItem.find(params[:id])
     if @cart_item.update(cart_item_params)
-      redirect_to cart_path, notice: "Quantity updated successfully."
+      redirect_to cart_items_path, notice: "Quantity updated successfully."
     else
       render :edit
     end
   end 
   
   def destroy
-    @item = Item.fimnd(params[:id])
-    @item.destroy
+    @cart_item = CartItem.fimnd(params[:id])
+    @cart_item.destroy
     redirect_to cart_items_path, notice: "削除されました。"
   end 
   
   def destroy_all_records
-    Items.destroy_all
+    current_customer.cart_items.destroy_all
     redirect_to cart_items_path, notice: "全てのカート内商品が削除されました。"
   end
   
