c2fb64f94214c31fae36aeaffb397e316f127e38
EC2 Default User <ec2-user@ip-172-31-12-31.ap-northeast-1.compute.internal>
2023-08-25 08:07:19 +0000

    [Fix]item-order_stop-cart_create

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,6 +3,11 @@
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "管理者ログイン中"
+    admin_path
+  end
+  
   def after_sign_out_path_for(resource)
     new_admin_session_path
   end
